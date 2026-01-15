0ea8ce459f64c72ba6c8ad153c899d39e278823f
roydenteh99 <roydenteh100@gmail.com>
2026-01-15 22:00:03 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import InputSlider from './UIComponent/InputSlider';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <InputSlider />
+    </div>
+  );
+}
+
+export default App;
